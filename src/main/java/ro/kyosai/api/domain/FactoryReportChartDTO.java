9675e3182602b60b3d82d92c8f47827616824d68
Mihail Cepraga <yahim09@gmail.com>
2025-05-22 16:21:08 +0000

    refactoring

--- src/main/java/ro/kyosai/api/domain/FactoryReportChartDTO.java
+++ src/main/java/ro/kyosai/api/domain/FactoryReportChartDTO.java
@@ -1,7 +1,21 @@
 package ro.kyosai.api.domain;
 
+import java.math.BigInteger;
+import java.util.List;
+
 import ro.kyosai.api.domain.Interface.FactoryReportChart;
 
-public record FactoryReportChartDTO(FactoryLineDTO factoryLineDTO) implements FactoryReportChart   {
+public record FactoryReportChartDTO(BigInteger id, String title, String shortTitle,
+                          GuageChartDTO oee, List<BarChartItem> production, List<DonutChartItem> plannedTime) implements FactoryReportChart {
+                            
+    public FactoryReportChartDTO(BigInteger id, String title, String shortTitle,
+            GuageChartDTO oee, List<BarChartItem> production, List<DonutChartItem> plannedTime) {
+        this.id = id;
+        this.title = title;
+        this.shortTitle = shortTitle;
+        this.oee = oee;
+        this.production = production;
+        this.plannedTime = plannedTime;
+    }
 
 }
