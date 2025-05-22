621f6e745b5c2bdf891135f705728f2a885216cf
Mihail Cepraga <yahim09@gmail.com>
2025-05-22 16:33:44 +0000

    refactoring

--- src/main/java/ro/kyosai/api/service/FactoryReportService.java
+++ src/main/java/ro/kyosai/api/service/FactoryReportService.java
@@ -1,15 +1,3 @@
 package ro.kyosai.api.service;
 
-import java.util.List;
-
-import ro.kyosai.api.domain.FactoryReportTotalsDTO;
-import ro.kyosai.api.entity.FactoryReport;
-
-public interface FactoryReportService {
-
-    List<FactoryReport> getAllFactoryReportsBetweenDate(String startDate, String endDate);
-    FactoryReportTotalsDTO getAllFactoryReportsSumBetweenDate(String startDate, String endDate);
-
-    void jdbcImplementationExample();
-
-}
+public interface FactoryReportService { }
