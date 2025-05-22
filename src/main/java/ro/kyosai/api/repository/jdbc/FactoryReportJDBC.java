6504ec628084d3d7ebf79dfa7a3d2ffc6e412e32
Mihail Cepraga <yahim09@gmail.com>
2025-05-22 16:24:19 +0000

    get Weight, Production data

--- src/main/java/ro/kyosai/api/repository/jdbc/FactoryReportJDBC.java
+++ src/main/java/ro/kyosai/api/repository/jdbc/FactoryReportJDBC.java
@@ -1,6 +1,9 @@
 package ro.kyosai.api.repository.jdbc;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,14 +18,16 @@ import ro.kyosai.api.domain.FactoryReportTotalsDTO;
 public class FactoryReportJDBC {
 
     private static final Logger logger = LoggerFactory.getLogger(FactoryReportJDBC.class);
-    private static final String FACTORY_REPORT_TOTALS = """
-            SELECT SUM(OEE) AS oee, SUM(Quality) AS quality, SUM(Availability) AS availability,
-            SUM(Performance) AS performance, SUM(Weight) AS weight FROM Factory_Reports
-            """;
 
+       private static final String FACTORY_REPORT_TOTALS = """
+            SELECT SUM(OEE) AS oee, SUM(Quality) AS quality, SUM(Availability) AS availability,
+                SUM(Performance) AS performance, SUM(Weight) AS weight
+            FROM Factory_Reports
+            """;
     private final JdbcTemplate jdbcTemplate;
 
     public FactoryReportTotalsDTO getFactoryReportTotals() {
+
         logger.info("Executing query: {}", FACTORY_REPORT_TOTALS);
 
         return jdbcTemplate.queryForObject(FACTORY_REPORT_TOTALS, (rs, rowNum) -> new FactoryReportTotalsDTO(
@@ -30,11 +35,11 @@ public class FactoryReportJDBC {
                 rs.getBigDecimal("quality"),
                 rs.getBigDecimal("availability"),
                 rs.getBigDecimal("performance"),
-                rs.getBigDecimal("weight")));
+                rs.getBigDecimal("weight")
+                ));
     }
     public FactoryReportTotalsDTO getFactoryReportTotalsBetween(final LocalDateTime startDate, final LocalDateTime endDate) {
-        String sql = FACTORY_REPORT_TOTALS + "WHERE datetime BETWEEN ? AND ?";
-
+        String sql = String.format("%s WHERE datetime BETWEEN ? AND ?", FACTORY_REPORT_TOTALS);
         logger.info("Executing query: {}", sql);
 
         return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new FactoryReportTotalsDTO(
@@ -42,7 +47,30 @@ public class FactoryReportJDBC {
                 rs.getBigDecimal("quality"),
                 rs.getBigDecimal("availability"),
                 rs.getBigDecimal("performance"),
-                rs.getBigDecimal("weight")), startDate, endDate);
+                rs.getBigDecimal("weight")
+                ), startDate, endDate);
+    } 
+
+    public Map<LocalDate, BigDecimal> getFactoryReportWeightBetween(final LocalDateTime startDate, final LocalDateTime endDate) {
+        String sql = """
+                    SELECT CAST(datetime AS DATE) AS day, SUM(weight) AS weight
+                        FROM Factory_Reports
+                        WHERE datetime BETWEEN ? AND ?
+                        GROUP BY CAST(datetime AS DATE)
+                        ORDER BY day 
+                    """;
+
+        logger.info("Executing query: {}", sql);
+
+        return jdbcTemplate.query(sql, rs -> {
+            Map<LocalDate, BigDecimal> result = new java.util.HashMap<>();
+            while (rs.next()) {
+                LocalDate day = rs.getDate("day").toLocalDate();
+                BigDecimal weight = rs.getBigDecimal("weight");
+                result.put(day, weight);
+            }
+            return result;
+        }, startDate, endDate);
     }
 
 }
