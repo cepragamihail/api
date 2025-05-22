94832f29e31f05ef76d3029ccd56356907ef5c9d
Mihail Cepraga <yahim09@gmail.com>
2025-05-22 16:19:54 +0000

    refactoring

--- src/main/java/ro/kyosai/api/domain/FactoryLineDTO.java
+++ src/main/java/ro/kyosai/api/domain/FactoryLineDTO.java
@@ -1,12 +1,13 @@
 package ro.kyosai.api.domain;
 
 import java.math.BigInteger;
+import java.util.List;
 
 public record FactoryLineDTO(BigInteger id, String title, String shortTitle,
-                          GuageChartDTO oee, BarChartDTO production, DonutChart plannedTime) {
+                          GuageChartDTO oee, List<?> production, DonutChartItem plannedTime) {
 
     public FactoryLineDTO( BigInteger id, String title, String shortTitle,
-                       GuageChartDTO oee, BarChartDTO production, DonutChart plannedTime) {
+                       GuageChartDTO oee, List<?> production, DonutChartItem plannedTime) {
         this.id = id;
         this.title = title;
         this.shortTitle = shortTitle;
