dc7b6b456590e1b62bf8b8aba8e717431ad379ce
Mihail Cepraga <yahim09@gmail.com>
2025-05-24 08:19:28 +0000

    add valueAlias parameter to BarChartItem

--- src/main/java/ro/kyosai/api/domain/BarChartItem.java
+++ src/main/java/ro/kyosai/api/domain/BarChartItem.java
@@ -2,13 +2,13 @@ package ro.kyosai.api.domain;
 
 import java.math.BigInteger;
 
-public record BarChartItem(String name, BigInteger value, String color, String unit) {
+public record BarChartItem(String name, BigInteger value, String valueAlias, String color, String unit) {
 
-    public BarChartItem(String name, BigInteger value, String color, String unit) {
+    public BarChartItem(String name, BigInteger value, String valueAlias, String color, String unit) {
         this.name = name;
         this.value = value;
+        this.valueAlias = valueAlias;
         this.color = color;
         this.unit = unit;
     }
-
 }
