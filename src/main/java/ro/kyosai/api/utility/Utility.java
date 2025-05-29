545c61e08132bc7a98380a511846edd7626e78a8
Mihail Cepraga <yahim09@gmail.com>
2025-05-29 16:03:54 +0000

    refactor: enhance Utility class with additional query generation methods and constants

--- src/main/java/ro/kyosai/api/utility/Utility.java
+++ src/main/java/ro/kyosai/api/utility/Utility.java
@@ -1,10 +1,30 @@
 package ro.kyosai.api.utility;
 
+import static java.util.stream.Collectors.joining;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
+import java.util.stream.Stream;
 
 public final class Utility {
+
+
+    public static final String SUM_ALIAS_PREFIX = "Sum";
+    public static final String DELIMITER = "_";
+    public static final String TABLE_ALIAS_PREFIX = "tar";
+    public static final String PERCENTAGE_UNITS = "%";
+
+    public static final String QUALITY = "quality";
+    public static final String PERFORMANCE = "performance";
+    public static final String AVAILABILITY = "availability";
+    public static final String OEE = "oee";
+    public static final String DATETIME = "datetime";
+    public static final String WEIGHT = "weight";
+    public static final String PRODUCT_ID = "productId";
+    public static final String AVAILABLE_TIMESTAMP = "availableTimestamp";
+    public static final String LOSS_TIMESTAMP = "lossTimestamp";
+    public static final String PRODUCTION_TIMESTAMP = "productionTimestamp";
 
     private Utility() {
         throw new AssertionError("Cannot instantiate utility class");
@@ -33,39 +53,101 @@ public final class Utility {
         return formatted + suffixes[magnitude];
     }
 
-    public static final String generateQueryByDbNameTableNameAndTableAlias(String tableName, String tableAlias, String dbName) {
-        return String.format("""
-            SELECT CONCAT(%1$s.Diameter,'_', %1$s.Width,'_', %1$s.Height,'_', %1$s.[Length],'_', %1$s.Thickness) as productId,
-            %1$s.Weight as weight,
-            %1$s.[Datetime] as datetime,
-            %1$s.OEE as oee,
-            %1$s.Availability as availability,
-            %1$s.Performance as performance,
-            %1$s.Quality as quality,
-            %1$s.Production_Timestamp as productionTimestamp,
-            %1$s.Loss_Timestamp as lossTimestamp,
-            %1$s.Available_Timestamp as availableTimestamp
-            FROM %3$s.dbo.%2$s %1$s
-            """, tableAlias, tableName, dbName);
+    public static final String generateQueryByDbNameTableNameAndTableAlias(String tableName, String tableAlias,
+            String dbName) {
+        return String.format(
+            """
+                SELECT CONCAT(%1$s.Diameter,'_', %1$s.Width,'_', %1$s.Height,'_', %1$s.[Length],'_', %1$s.Thickness) as %2$s,
+                %1$s.Weight as %3$s,
+                %1$s.[Datetime] as %4$s,
+                %1$s.OEE as %5$s,
+                %1$s.Availability as %6$s,
+                %1$s.Performance as %7$s,
+                %1$s.Quality as %8$s,
+                %1$s.Production_Timestamp as %9$s,
+                %1$s.Loss_Timestamp as %10$s,
+                %1$s.Available_Timestamp as %11$s
+                FROM %13$s.dbo.%12$s %1$s
+                """,
+            tableAlias,
+            PRODUCT_ID,
+            WEIGHT,
+            DATETIME,
+            OEE,
+            AVAILABILITY,
+            PERFORMANCE,
+            QUALITY,
+            PRODUCTION_TIMESTAMP,
+            LOSS_TIMESTAMP,
+            AVAILABLE_TIMESTAMP,
+            tableName,
+            dbName
+        );
     }
 
-    public static final List<String> getAllQueriesForAllTablesWithUniqueAliases(List<String> tableNames, String dbName) {
-        int[] idx = {1};
+    public static final List<String> getAllQueriesForAllTablesWithUniqueAliases(List<String> tableNames,
+            String dbName, String tableAliasPrefix) {
+        int[] idx = { 1 };
         return tableNames.stream()
-            .map(table -> generateQueryForDbNameTableNameAndTableAlias(table, idx[0]++, dbName))
-            .toList();
+                .map(table -> generateQueryForDbNameTableNameAndTableAlias(table, tableAliasPrefix + idx[0]++, dbName))
+                .toList();
     }
+    
 
-    public static final String generateQueryForDbNameTableNameAndTableAlias(String tableName, int aliasIndex, String dbName) {
-        return generateQueryByDbNameTableNameAndTableAlias(tableName, "sr" + aliasIndex, dbName); // Use a unique alias based on the index
+    public static final String generateQueryForDbNameTableNameAndTableAlias(String tableName, String tableAlias,
+            String dbName) {
+        return generateQueryByDbNameTableNameAndTableAlias(tableName, tableAlias, dbName);
     }
 
     public static String generateUnionOfQueries(List<String> queries) {
         return String.join(" UNION ALL ", queries);
     }
 
-    public static final String getQueryWithWhereCaluseBetwenDates(String query, String tableAlias) {
-        return String.format("%s WHERE %s.datetime BETWEEN ? AND ? ", query, tableAlias);
+    public static final String getQueryWithWhereCaluseBetwenDates(String query, String tableAlias, String columnName) {
+        return String.format("%s WHERE %s.%s BETWEEN ? AND ? ", query, tableAlias, columnName);
     }
 
+    public static final String getProductIdColumnName(String tableAlias) {
+        return "CONCAT(" + 
+                Stream.of(FactoryReportColumn.getProductUniqueColumns())
+                .map(column -> tableAlias + "." + column)
+                .collect(joining(DELIMITER)) +
+                ") as " + PRODUCT_ID;
+    }
+    public static final String[] getColumnsNameAndAliasWithSumFunctions(String alias, FactoryReportColumn... columns) {
+        return Stream.of(columns)
+                .map(column -> String.format("SUM(%s.%s) as %sSum", alias, column.getColumnName(), column.getAlias()))
+                .toArray(String[]::new);
+    }
+    public static final String getColumnWithSumFunctions(String columnName, String columnAlias) {
+        return String.format("SUM(%s) as %sSum", columnName, columnAlias);
+    }
+    public static final String[] getColumnsWithSumFunctions(String... columsName) {
+        return Stream.of(columsName)
+                .map(column -> getColumnWithSumFunctions(column, column))
+                .toArray(String[]::new);
+    }
+    public static final String[] getColumnsWithSumFunctions(FactoryReportColumn... columns) {
+        return Stream.of(columns)
+                .map(column -> getColumnWithSumFunctions(column.getColumnName(), column.getAlias()))
+                .toArray(String[]::new);
+    }
+    public static final String getColumnWhitsCastASDate(String columnName, String alias) {
+        return String.format("CAST(%s AS DATE) AS %s", columnName, alias);
+    }
+
+    public static final String getTableNameWithAliasAndDBName(String dbName ,String tableName, String alias) {
+        return String.format("%s.dbo.%s %s", dbName, tableName, alias);
+    }
+    public static final String[] getColumnsNameWithTableAlias(String tableAlias, String... columns) {
+        return Stream.of(columns)
+                .map(column -> tableAlias + "." + column)
+                .toArray(String[]::new);
+    }
+
+    public static final String[] getColumnsNameWithTableAlias(String tableAlias, FactoryReportColumn... columns) {
+        return Stream.of(columns)
+                .map(column -> tableAlias + "." + column.getColumnName())
+                .toArray(String[]:: new);
+    }
 }
