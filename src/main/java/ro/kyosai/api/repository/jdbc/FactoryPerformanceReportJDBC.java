dc939a031064706c6f1af82c573f9ab3233d76b5
Mihail Cepraga <yahim09@gmail.com>
2025-05-29 16:01:21 +0000

    refactor: enhance performance sum query with dynamic column selection and table handling

--- src/main/java/ro/kyosai/api/repository/jdbc/FactoryPerformanceReportJDBC.java
+++ src/main/java/ro/kyosai/api/repository/jdbc/FactoryPerformanceReportJDBC.java
@@ -1,29 +1,52 @@
 package ro.kyosai.api.repository.jdbc;
 
+import static ro.kyosai.api.utility.FactoryReportColumn.getPerformanceColumns;
+import static ro.kyosai.api.utility.FactoryReportColumn.getPerformanceColumnsEnum;
+import static ro.kyosai.api.utility.FactoryReportColumn.getPerformanceColumnsEnumWithDatetime;
+import static ro.kyosai.api.utility.Utility.SUM_ALIAS_PREFIX;
+import static ro.kyosai.api.utility.Utility.getColumnsNameWithTableAlias;
+import static ro.kyosai.api.utility.Utility.getTableNameWithAliasAndDBName;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import lombok.AllArgsConstructor;
 import ro.kyosai.api.domain.ReportPerformanceDTO;
 import ro.kyosai.api.domain.ReportPerformanceRecord;
+import ro.kyosai.api.utility.FactoryReportColumn;
+import ro.kyosai.api.utility.QueryBuilder;
+import ro.kyosai.api.utility.Utility;
 
-@AllArgsConstructor
 @Component
 public class FactoryPerformanceReportJDBC {
     private static final Logger log = LoggerFactory.getLogger(FactoryPerformanceReportJDBC.class);
+
+        private static final String TABLE_ALIAS_PREFIX = "tar";
+
 
     private static final String FACTORY_PERFORMANCE_REPORT = """
             SELECT fpr.productId, fpr.datetime, fpr.Available_Timestamp, fpr.Production_Timestamp, fpr.Loss_Timestamp
             FROM Mittal_Reports.dbo.Factory_Performance_Reports fpr
             """;
 
+
+    @Value("#{'${app.datasource.table.names:SRR60_Reports}'.split(',')}")
+    private List<String> tableNames;
+    
+    @Value("${app.datasource.dbname:Mittal_Reports}")
+    private String dbName;
+
     final JdbcTemplate jdbcTemplate;
+        public FactoryPerformanceReportJDBC(JdbcTemplate jdbcTemplate) {
+                this.jdbcTemplate = jdbcTemplate;
+        }
 
     public Optional<List<ReportPerformanceDTO>> getFactoryPerformanceReport() {
         log.info("Executing query: {}", FACTORY_PERFORMANCE_REPORT);
@@ -48,20 +71,39 @@ public class FactoryPerformanceReportJDBC {
                         rs.getInt("Loss_Timestamp")), startDate, endDate));
     }
 
-    public List<ReportPerformanceRecord> getPerformanceSumBetween(final LocalDateTime startDate, final LocalDateTime endDate) {
-        String query = """
-                SELECT SUM(fpr.Available_Timestamp) as availableTimestamp, 
-                        SUM(fpr.Production_Timestamp) as productionTimestamp, 
-                        SUM(fpr.Loss_Timestamp) as lossTimestamp
-                FROM Mittal_Reports.dbo.Factory_Performance_Reports fpr
-                WHERE fpr.datetime BETWEEN ? AND ?    
-                        """;
-        log.info("Executing query: {}", query);
+public List<ReportPerformanceRecord> getPerformanceSumBetween(final LocalDateTime startDate, final LocalDateTime endDate) {
+        String[] performanceColumns = getPerformanceColumns();
+        String[] columnsWithSumFunctions = Utility.getColumnsWithSumFunctions(performanceColumns);
+
+        String query = new QueryBuilder().select(columnsWithSumFunctions)
+                .fromSubquery(getUnionAllTablePerformanceQuery(tableNames, dbName))
+                .where( FactoryReportColumn.DATETIME.getColumnName() + " BETWEEN ? AND ? ")
+                .build();
+
+        for (FactoryReportColumn performanceColumnsEnum : getPerformanceColumnsEnum()) {
+                log.info("Column: {}, Alias: {}", performanceColumnsEnum.getColumnName(), performanceColumnsEnum.getAlias());
+        }
         return jdbcTemplate.query(query,
                 (rs, rowNum) -> new ReportPerformanceRecord(
-                        rs.getInt("availableTimestamp"),
-                        rs.getInt("productionTimestamp"),
-                        rs.getInt("lossTimestamp")), startDate, endDate);
+                        rs.getInt(FactoryReportColumn.AVAILABLE_TIMESTAMP.getColumnName() + SUM_ALIAS_PREFIX),
+                        rs.getInt(FactoryReportColumn.PRODUCTION_TIMESTAMP.getColumnName() + SUM_ALIAS_PREFIX),
+                        rs.getInt(FactoryReportColumn.LOSS_TIMESTAMP.getColumnName() + SUM_ALIAS_PREFIX)
+                        ), startDate, endDate);
+    }
+
+    public String getUnionAllTablePerformanceQuery(List<String> tableNames, String dbName) {
+        int[] idx = { 1 };
+        return tableNames.stream()
+                .map(table -> getPerfomanceQueryByTableName(table, TABLE_ALIAS_PREFIX + idx[0]++, dbName))
+                .collect(Collectors.joining(" UNION ALL "));
+    }
+
+
+    public String getPerfomanceQueryByTableName(String tableName, String tableAlias, String dbName) {
+        return new QueryBuilder()
+                        .select(getColumnsNameWithTableAlias(tableAlias, getPerformanceColumnsEnumWithDatetime()))
+                        .from(getTableNameWithAliasAndDBName(dbName, tableName, tableAlias))
+                        .build();
     }
 
 }
