57716ff05613a48275479f6c1c6389ee2347192b
Mihail Cepraga <yahim09@gmail.com>
2025-05-29 16:01:42 +0000

    refactor: improve FactoryReportJDBC with dynamic table handling and enhanced query methods

--- src/main/java/ro/kyosai/api/repository/jdbc/FactoryReportJDBC.java
+++ src/main/java/ro/kyosai/api/repository/jdbc/FactoryReportJDBC.java
@@ -1,76 +1,110 @@
 package ro.kyosai.api.repository.jdbc;
 
+import static ro.kyosai.api.utility.FactoryReportColumn.AVAILABILITY;
+import static ro.kyosai.api.utility.FactoryReportColumn.DATETIME;
+import static ro.kyosai.api.utility.FactoryReportColumn.OEE;
+import static ro.kyosai.api.utility.FactoryReportColumn.PERFORMANCE;
+import static ro.kyosai.api.utility.FactoryReportColumn.QUALITY;
+import static ro.kyosai.api.utility.FactoryReportColumn.WEIGHT;
+import static ro.kyosai.api.utility.FactoryReportColumn.getGuageAnalisisColumnsEnum;
+import static ro.kyosai.api.utility.FactoryReportColumn.getGuageAnalisisColumnsEnumWithDatetime;
+import static ro.kyosai.api.utility.Utility.SUM_ALIAS_PREFIX;
+import static ro.kyosai.api.utility.Utility.TABLE_ALIAS_PREFIX;
+import static ro.kyosai.api.utility.Utility.getColumnWhitsCastASDate;
+import static ro.kyosai.api.utility.Utility.getColumnsNameWithTableAlias;
+import static ro.kyosai.api.utility.Utility.getTableNameWithAliasAndDBName;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-import lombok.AllArgsConstructor;
 import ro.kyosai.api.domain.FactoryReportTotalsDTO;
+import ro.kyosai.api.utility.FactoryReportColumn;
+import ro.kyosai.api.utility.QueryBuilder;
+import ro.kyosai.api.utility.Utility;
 
-@AllArgsConstructor
-@Component
+@Repository
 public class FactoryReportJDBC {
 
-    private static final Logger logger = LoggerFactory.getLogger(FactoryReportJDBC.class);
+    private static final String DAY_ALIAS = "day";
 
-       private static final String FACTORY_REPORT_SUM = """
-            SELECT SUM(OEE) AS oee, SUM(Quality) AS quality, SUM(Availability) AS availability,
-                SUM(Performance) AS performance, SUM(Weight) AS weight
-            FROM Factory_Reports
-            """;
+    private static final Logger log = LoggerFactory.getLogger(FactoryReportJDBC.class);
+
+    @Value("#{'${app.datasource.table.names:SRR60_Reports}'.split(',')}")
+    private List<String> tableNames;
+    
+    @Value("${app.datasource.dbname:Mittal_Reports}")
+    private String dbName;
+
     private final JdbcTemplate jdbcTemplate;
 
-    public FactoryReportTotalsDTO getFactoryReportSum() {
-
-        logger.info("Executing query: {}", FACTORY_REPORT_SUM);
-
-        return jdbcTemplate.queryForObject(FACTORY_REPORT_SUM, (rs, rowNum) -> new FactoryReportTotalsDTO(
-                rs.getBigDecimal("oee"),
-                rs.getBigDecimal("quality"),
-                rs.getBigDecimal("availability"),
-                rs.getBigDecimal("performance"),
-                rs.getBigDecimal("weight")
-                ));
+    public FactoryReportJDBC(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
-    public FactoryReportTotalsDTO getFactoryReportTotalsBetween(final LocalDateTime startDate, final LocalDateTime endDate) {
-        String sql = String.format("%s WHERE datetime BETWEEN ? AND ?", FACTORY_REPORT_SUM);
-        logger.info("Executing query: {}", sql);
 
-        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new FactoryReportTotalsDTO(
-                rs.getBigDecimal("oee"),
-                rs.getBigDecimal("quality"),
-                rs.getBigDecimal("availability"),
-                rs.getBigDecimal("performance"),
-                rs.getBigDecimal("weight")
-                ), startDate, endDate);
+    public FactoryReportTotalsDTO getFactoryReportOfGuageAnalisisChartBetween(final LocalDateTime startDate, final LocalDateTime endDate) {
+
+        String unionAllQuery = this.getUnionAllTableQuery(tableNames, dbName, getGuageAnalisisColumnsEnumWithDatetime());
+        String[] guageAnalisisSumColumns = Utility.getColumnsWithSumFunctions(getGuageAnalisisColumnsEnum());
+        String query = new QueryBuilder().select(guageAnalisisSumColumns)
+            .fromSubquery(unionAllQuery)
+            .where(DATETIME.getColumnName() + " BETWEEN ? AND ?")
+            .build();
+        log.info("Executing query: {}", query);
+        return jdbcTemplate.queryForObject(query, (rs, rowNum) -> new FactoryReportTotalsDTO(
+            rs.getBigDecimal(OEE.getAlias() + SUM_ALIAS_PREFIX),
+            rs.getBigDecimal(QUALITY.getAlias() + SUM_ALIAS_PREFIX),
+            rs.getBigDecimal(AVAILABILITY.getAlias() + SUM_ALIAS_PREFIX),
+            rs.getBigDecimal(PERFORMANCE.getAlias() + SUM_ALIAS_PREFIX),
+            rs.getBigDecimal(WEIGHT.getAlias() + SUM_ALIAS_PREFIX)
+        ), startDate, endDate);
     }
 
     public Map<LocalDate, BigDecimal> getFactoryReportWeightBetween(final LocalDateTime startDate, final LocalDateTime endDate) {
-        String sql = """
-                    SELECT CAST(datetime AS DATE) AS day, SUM(weight) AS weight
-                        FROM Factory_Reports
-                        WHERE datetime BETWEEN ? AND ?
-                        GROUP BY CAST(datetime AS DATE)
-                        ORDER BY day 
-                    """;
+        String unionAllQuery = this.getUnionAllTableQuery(tableNames, dbName, DATETIME, WEIGHT);
+        String datetimeWithCastColumn = getColumnWhitsCastASDate(DATETIME.getColumnName(), DAY_ALIAS);
+        String weightSumColumns = Utility.getColumnWithSumFunctions(WEIGHT.getColumnName(), WEIGHT.getAlias());
+        // Add the datetime column with cast to 
 
-        logger.info("Executing query: {}", sql);
-
-        return jdbcTemplate.query(sql, rs -> {
+        String query = new QueryBuilder().select(datetimeWithCastColumn, weightSumColumns)
+            .fromSubquery(unionAllQuery)
+            .where(DATETIME.getColumnName() + " BETWEEN ? AND ?")
+            .groupBy(String.format("CAST(%s AS DATE)", DATETIME.getColumnName()))
+            .orderBy(DAY_ALIAS, "ASC")
+            .build();
+        log.info("Executing query: {}", query);
+        return jdbcTemplate.query(query, rs -> {
             Map<LocalDate, BigDecimal> result = new java.util.HashMap<>();
             while (rs.next()) {
-                LocalDate day = rs.getDate("day").toLocalDate();
-                BigDecimal weight = rs.getBigDecimal("weight");
+                LocalDate day = rs.getDate(DAY_ALIAS).toLocalDate();
+                BigDecimal weight = rs.getBigDecimal(WEIGHT.getAlias() + SUM_ALIAS_PREFIX);
                 result.put(day, weight);
             }
             return result;
         }, startDate, endDate);
+    }
+
+    public String getUnionAllTableQuery(List<String> tableNames, String dbName, FactoryReportColumn... columns) {
+        int[] idx = { 1 };
+        return tableNames.stream()
+                .map(table -> getQueryByTableName(table, TABLE_ALIAS_PREFIX + idx[0]++, dbName, columns))
+                .collect(Collectors.joining(" UNION ALL "));
+    }
+
+    public String getQueryByTableName(String tableName, String tableAlias, String dbName, FactoryReportColumn... columns) {
+        return new QueryBuilder()
+                        .select(getColumnsNameWithTableAlias(tableAlias, columns))
+                        .from(getTableNameWithAliasAndDBName(dbName, tableName, tableAlias))
+                        .build();
     }
 
 }
